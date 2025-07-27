bbff846869b072c403f102c4c1f2717e7c08b167
Bharath Namballa <bcelluloid@gmail.com>
2025-07-27 17:46:33 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -74,7 +74,7 @@ const Footer = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-primary" />
-                <span className="text-gray-300"></span>
+                <span className="text-gray-300">svrpoultryequipments@gmail.com</span>
               </div>
             </div>
           </div>
