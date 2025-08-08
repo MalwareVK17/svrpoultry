41c6b1e3d8a93e37b7162e43cd933f10402f413b
Bharath Namballa <bcelluloid@gmail.com>
2025-08-08 14:57:11 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -71,7 +71,7 @@ const Footer = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <Phone className="w-5 h-5 text-primary" />
-                <span className="text-gray-300">+91 98493 97021 </span>
+                <span className="text-gray-300">+91 88866 45122 </span>
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-primary" />
