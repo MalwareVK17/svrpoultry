6b45dba109dac953705673fcaaa2f6f5ed3e19e8
Bharath Namballa <bcelluloid@gmail.com>
2025-08-11 21:44:32 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
           <div className="space-y-4">
             <div className="flex items-center space-x-3">
               <div className="w-10 h-10 rounded-full overflow-hidden flex items-center justify-center">
-                <img src="https://image2url.com/images/1754817461627-a4840edd-6afb-420b-bcfa-61e0ed62048a.png" alt="SVR Logo" className="w-full h-full object-contain" />
+                <img src="https://image2url.com/images/1754928767046-3ab6b0cf-af06-41c4-9e5e-0b938b85715d.png" alt="SVR Logo" className="w-full h-full object-contain" />
               </div>
               <span className="font-bold text-xl">SVR Poultry Equipments</span>
             </div>
