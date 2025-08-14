d00a66e747d6ba763fc9d9483744315875edc542
Bharath Namballa <bcelluloid@gmail.com>
2025-08-14 22:54:01 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -188,9 +188,6 @@ const Home = () => {
         </div>
       </section>
 
-      {/* Timeline Section */}
-      <Timeline />
-
       {/* Enhanced Partners Section */}
       <section className="py-24 bg-gradient-to-br from-gray-50 to-blue-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
