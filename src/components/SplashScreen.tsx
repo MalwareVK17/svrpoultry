f9c279b25d2f73b0ccb8aa1d6876aafe4ceddf81
Bharath Namballa <bcelluloid@gmail.com>
2025-08-16 11:24:59 +0530

    Update SplashScreen.tsx

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -35,7 +35,7 @@ const SplashScreen = ({ onComplete }: SplashScreenProps) => {
         >
           <div className="w-20 h-20 bg-white rounded-full flex items-center justify-center mx-auto mb-6 shadow-2xl p-2">
             <img 
-              src="/lovable-uploads/8ab94526-304b-43f1-8252-c09362b75e5d.png" 
+              src="https://image2url.com/images/1755323630651-936d4627-a344-45e9-a64c-c9dc78a70ea0.jpg" 
               alt="SVR Poultry Equipments Logo"
               className="w-full h-full object-contain"
             />
